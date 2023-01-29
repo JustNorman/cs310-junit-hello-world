24b7954c653db6eeed65604e1f9b30f7e79c22ec
JustNorman <norman2and2@gmail.com>
2023-01-29 16:34:25 -0600

    Intial Comit

---
+++ OneDrive/Documents/NetBeansProjects/NetBeansGitTest/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author norma
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(Test.testMethod1());
+    }
+    
+}
---
+++ OneDrive/Documents/NetBeansProjects/NetBeansGitTest/src/Test.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author norma
+ */
+public class Test {
+
+    public static String testMethod1()
+    {
+        return "Hello, World!";
+    }    
+}
